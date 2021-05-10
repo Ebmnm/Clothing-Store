f7bea3b1d15d8f4ebe28ac5e33baaaea110e9175
ebmnm <ebmnm@aol.com>
2021-05-10 19:01:04 -0400

    home page items

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { Component } from 'react';
 import React from "react"
-import HomePage from "./homepage.component"
+import HomePage from "./pages/homepage.component"
 
 
 function App() {
